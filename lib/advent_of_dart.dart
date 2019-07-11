949b0a3e228385ee6699bdd2df014ecfa4f35d0e
Mafinar K <mafinar@gmail.com>
2019-07-11 13:26:38 -0400

    Add generic message in currently function instead of impl

--- lib/advent_of_dart.dart
+++ lib/advent_of_dart.dart
@@ -1,4 +1,2 @@
-import 'package:advent_of_dart/year_2015/solutions.dart' as y2015;
-// import 'package:advent_of_dart/year_2018/solutions.dart' as y2018;
-
-currently() => y2015.day_1_part_1();
+currently() =>
+    "Import the package and run the function (i.e. problem_x_x()) to see results";
