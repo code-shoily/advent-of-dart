d3ce9fa1c88ee3c0c76a9ad81676c0f3fb53e748
Mafinar K <mafinar@gmail.com>
2019-07-15 13:29:13 -0400

    Add a file structure generator script

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 My attempts to solve [Advent of Code](https://adventofcode.com/) puzzles with the [Dart](http://dartlang.org).
 
-To run the last puzzle I successfully solved, run: `pub run bin/main.dart`
-
 All solutions codes are located in `lib/year_<year>/day_<day>` where `year` is the year and `day` is the day of the year I'm solving. `solutions.dart` exports all days for shorter test files.
 
 The input files are located in `lib/data/<year>/data_<day>_<part>.txt` where `part` is the part of the `day` that `year` the data belongs. So far I only encountered same dataset for both the parts but you never know.
+
+Or you can run `pub run bin/generate.dart <year> <day>` to generate some boilerplate for you.
+
+Run `pub run test` to run all the tests.
---
+++ bin/generate.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+const int latestYear = 2018;
+
+/// Generate code stub for main code for AoC [year] [day]
+generateCode(int year, int day) => '''
+/// Code for the solution of $year AoC, day $day.
+///
+/// Problem description can be seen [here](https://adventofcode.com/$year/day/$day)
+import 'package:advent_of_dart/helpers.dart';
+
+/// Read the raw [String] content from file and convert it to
+/// [List<String>].
+List<String> _processInput() =>
+    readFromFiles(year: $year, day: $day, part: 1).split('\\n').toList();
+
+day_1_part_1() => _processInput();
+day_1_part_2() => _processInput();
+''';
+
+/// Parse the argument and get year and day values. year should
+/// be between `2015` and [latestYear] and days within 1 to 25.
+List<int> parseArgument(List<String> argumentList) {
+  final int year = int.parse(argumentList[0]);
+  final int day = int.parse(argumentList[1]);
+  if (year > latestYear || year < 2015) throw ArgumentError();
+  if (day < 1 || day > 25) throw ArgumentError();
+  return [day, year];
+}
+
+main(List<String> arguments) {
+  try {
+    var parsedData = parseArgument(arguments);
+    int year = parsedData[0];
+    int day = parsedData[1];
+
+    var dataFile = File('lib/data/$year/day_${day}_1.txt');
+    var codeFile = File('lib/year_$year/day_${day}.dart');
+
+    if (!dataFile.existsSync()) dataFile.create(recursive: true);
+    if (!codeFile.existsSync()) {
+      codeFile.create(recursive: true);
+      codeFile.writeAsStringSync(generateCode(year, day));
+    }
+  } on FormatException catch (_) {
+    print("Please enter valid number for <year> <day>");
+  } on ArgumentError catch (_) {
+    print("Invalid values for year (2015-$latestYear) or day (1-25)");
+  } catch (_) {
+    print("Usage: `pub run bin/generate.dart year day`");
+  }
+}
